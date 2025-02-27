06c18c5d801b9951f0dbba1e656aa2de20f8bfb9
Matteo Mazzarini <matteo.mazzarini.91@gmail.com>
2025-02-27 22:52:33 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,2 +1,31 @@
 project "ImGui"
     kind "Staticlib"
+    language "C++"
+
+        targetidr ("bin/" .. outputdir .. "/%{prj.name})
+        objdir ("bin-int/" .. outputdir .. "/%{prj.name})
+
+        files
+        {
+            imconfig.h
+            imgui.h
+            imgui.cpp
+            imgui_demo.cpp
+            imgui_draw.cpp
+            imgui_internal.h
+            imgui_tables.cpp
+            imgui_widgets.cpp
+            imstb_rectpack.h
+            imstb_textedit.h
+            imstb_truetype.h
+        }
+
+        filter "system:windows"
+            systemversion "latest"
+            cppdialect "C++17"
+            staticruntime "On"
+
+        filter {"system:windows", "configurations:Release"}
+            buildoptions "/MT"
+
+        
